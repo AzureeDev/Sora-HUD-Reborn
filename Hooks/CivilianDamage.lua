08cb8249c4c0d04c50f8e9f72f68a0f81126c5a8
Nepgearsy <timertimer5@gmail.com>
2018-08-24 11:44:40 +0200

    Fix civ timer update if you do the kill

--- Hooks/CivilianDamage.lua
+++ Hooks/CivilianDamage.lua
@@ -1,3 +1,7 @@
-NepHook:Post(CivilianDamage, "die", function(self, variant)
-    managers.hud._hud_assault_corner:UpdateCivKilled()
+NepHook:Post(CivilianDamage, "_on_damage_received", function(self, damage_info)
+    local attacker_unit = damage_info and damage_info.attacker_unit
+
+    if damage_info.result.type == "death" and attacker_unit == managers.player:player_unit() then
+        managers.hud._hud_assault_corner:UpdateCivKilled()
+    end
 end)
